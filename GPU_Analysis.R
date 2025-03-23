0767540db2456e7c546011ed4df76a82c0578ea4
jamie chen <jamie950315@gmail.com>
2025-03-23 10:25:53 +0800

    add csv files

---
+++ GPU_Analysis.R
@@ -0,0 +1,8 @@
+intelCSV<-read.csv("Intel.csv")
+amdCSV<-read.csv("AMD.csv")
+nvidiaCSV<-read.csv("NVIDIA.csv")
+
+df.intel<-data.frame(intelCSV)
+df.amd<-data.frame(amdCSV)
+df.nvidia<-data.frame(nvidiaCSV)
+
